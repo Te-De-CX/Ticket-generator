9714aa38f7c95fe941ead2bd760ec1325c768903
teslim <tunjiheritage@gmail.com>
2025-02-21 17:40:50 +0100

    first commit

---
+++ ticket-generator/src/components/Form.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <>
+        <section>
+            <nav>
+                <img src="" alt="" />
+                <p>coding conf</p>
+            </nav>
+            <header>
+                <h1>
+                    your journey to coding conf 2025 starts here!
+                </h1>
+                <p>secure your spot at next year's biggest coding conference.</p>
+            </header>
+            <main>
+                <div>
+                    <label htmlFor="">upload avatar</label>
+                    <input type="image" />
+                    <p>"warning" upload your photo (jpg or png, max size: 500KB).</p>
+                </div>
+                <div>
+                    <label htmlFor="">full name</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label htmlFor="">email address</label>
+                    <input type="email" />
+                </div>
+                <div>
+                    <label htmlFor="">github username</label>
+                    <input type="text" />
+                </div>
+                <button>'
+                    Generate My ticket
+                </button>
+            </main>
+        </section>
+    </>
+  )
+}
